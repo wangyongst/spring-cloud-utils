c4170cc00e408ac0179de9327f958391003d3d5d
wangyongst <wangyongst@gmail.com>
2019-05-16 12:26:46 +0800

    create

--- src/main/java/com/spring/cloud/utils/controller/DbController.java
+++ src/main/java/com/spring/cloud/utils/controller/DbController.java
@@ -4,6 +4,8 @@ import com.spring.cloud.utils.utils.db.IdUtils;
 import com.spring.cloud.utils.utils.db.TimeUtils;
 import com.spring.cloud.utils.utils.result.Result;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,13 +17,16 @@ public class DbController {
 
     @ApiOperation(value = "生成31位长度ID")
     @GetMapping(value = "createId")
-    public Result ok() {
+    public Result createId() {
         return IdUtils.createId();
     }
 
     @ApiOperation(value = "时间统一格式化为yyyy-MM-dd HH:mm:ss")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "time", value = "要格式化的时间（必需，类型为long）", required = true)
+    })
     @GetMapping(value = "formatTime")
-    public Result ok(long time) {
+    public Result formatTime(long time) {
         return TimeUtils.format(time);
     }
 }
